3b01e6c8e0b3ef29553c8df8e6802fe2bd0ec1ab
OllyButters <olly.butters@bristol.ac.uk>
2014-09-21 16:14:22 +0100

    split out into multiple files and reworked inst aggregating

--- source/papers.py
+++ source/papers.py
@@ -4,14 +4,15 @@
 #Get all the paper metadata from pubmed and do stuff with it
 ##########################################################
 
-#12/9/14
+#21/9/14
 
 import csv
 import json
 import os.path
-import gdata.docs.service
+#import gdata.docs.service
 
 import tools
+import analysis
 
 from Bio import Entrez
 Entrez.email = "olly.butters@bristol.ac.uk"     # Always tell NCBI who you are
@@ -95,6 +96,11 @@ for this_pmid in pmids:
     this_paper['pmid']=this_pmid
     this_paper['ArticleTitle'] = record['MedlineCitation']['Article']['ArticleTitle']
     this_paper['Journal'] = record['MedlineCitation']['Article']['Journal']['ISOAbbreviation']
+
+    try:
+        this_paper['AbstractText'] = record['MedlineCitation']['Article']['Abstract']['AbstractText']
+    except:
+        print 'No Abstract text'
     
     #Mesh keywords
     try:
@@ -120,173 +126,17 @@ fo = open(file_name, 'wb')
 fo.write(json.dumps(papers, indent=4))
 fo.close()
 
-print '####All papers'
+#print '####All papers'
 #print papers
 
 print str(num_pmids)+' PMIDs to process'
 print str(len(papers))+' papers processed'
 
 
-############################################################
-#Have all the data now, so do something with it
-
-############################################################
-#Build a list of all journals and count frequencies of each
-print "\n###Journals###"
-
-journals = []
-for this_pmid in pmids:
-    journals.append(papers[this_pmid]['Journal'])
-
-print str(len(journals))+'/'+str(num_pmids)
-print str(len(set(journals)))+' different journals'
-
-#calculate the frequency of each journal
-freq = dict((x,journals.count(x)) for x in set(journals))
-
-i=0
-print 'Top 5'
-
-#print a list of sorted frequencies
-with open('../data/journals.csv', 'wb') as csvfile:
-    journals_file = csv.writer(csvfile)
-    for w in sorted(freq, key=freq.get, reverse=True):
-        if i<5:
-            print w, freq[w]
-            i=i+1
-        journals_file.writerow([w.encode('utf-8'), freq[w]])
-
-
-
-
-
-############################################################
-#Try with the authors - these are in a nested dict
-authors = []
-for this_pmid in pmids:
-    #print this_pmid
-    for this_author in papers[this_pmid]['AuthorList']:
-        #There are some entries in the author list that are not actually authors e.g. 21379325 last author
-        try:
-            authors.append(this_author['LastName'])
-        except:
-            pass
-
-
-#print authors
-freq = dict((x,authors.count(x)) for x in set(authors))
-print "\n###Authors###"
-
-print str(len(set(authors)))+' different authors'
-#print freq
-
-i=0
-print 'Top 5'
-
-with open('../data/authors.csv', 'wb') as csvfile:
-    authors_file = csv.writer(csvfile)
-    for w in sorted(freq, key=freq.get, reverse=True):
-        if i<5:
-            print w, freq[w]
-            i=i+1
-        #Need to utf-8 encode
-        authors_file.writerow([w.encode('utf-8'), freq[w]])
-
-
-############################################################
-#Try with the FIRST authors - these are in a nested dict
-first_authors = []
-for this_pmid in pmids:
-    first_authors.append(papers[this_pmid]['AuthorList'][0]['LastName'])
-
-#print authors
-freq = dict((x,first_authors.count(x)) for x in set(first_authors))
-print "\n###First authors###"
-
-print str(len(first_authors))+'/'+str(num_pmids)
-print str(len(set(first_authors)))+' different first authors'
-
-#print freq
-
-i=0
-print 'Top 5'
-
-with open('../data/first_authors.csv', 'wb') as csvfile:
-    authors_file = csv.writer(csvfile)
-    for w in sorted(freq, key=freq.get, reverse=True):
-        if i<5:
-            print w, freq[w]
-            i=i+1
-        #Need to utf-8 encode
-        authors_file.writerow([w.encode('utf-8'), freq[w]])
-
-############################################################
-#Try with the FIRST authors INSTITUTE- these are in a nested dict
-first_authors_inst = []
-for this_pmid in pmids:
-    try:
-        first_authors_inst.append(papers[this_pmid]['AuthorList'][0]['Affiliation'])
-    except:
-        pass
-
-tools.clean_institution(first_authors_inst)
-
-#print authors
-freq = dict((x,first_authors_inst.count(x)) for x in set(first_authors_inst))
-print "\n###First authors institute###"
-
-print str(len(first_authors_inst))+'/'+str(num_pmids)
-print str(len(set(first_authors_inst)))+' different first author institutes'
-#print freq
-
-i=0
-print 'Top 5'
-
-with open('../data/first_authors_inst.csv', 'wb') as csvfile:
-    authors_file = csv.writer(csvfile)
-    for w in sorted(freq, key=freq.get, reverse=True):
-        if i<5:
-            print w, freq[w]
-            i=i+1
-        #Need to utf-8 encode
-        authors_file.writerow([w.encode('utf-8'), freq[w]])
-
-
-
-############################################################
-#Try with the mesh headings - these are in a nested dict
-mesh = []
-coverage=0
-for this_pmid in pmids:
-    #print this_pmid
-    
-    if 'MeshHeadingList' in papers[this_pmid]:
-        coverage = coverage + 1
-    
-    try:
-        for this_mesh in papers[this_pmid]['MeshHeadingList']:
-            mesh.append(this_mesh['DescriptorName'])
-    except:
-        #Do nothing
-        pass
-
-#print mesh
-freq = dict((x,mesh.count(x)) for x in set(mesh))
-print "\n###Mesh###"
-
-#print freq
-
-print str(coverage)+'/'+str(num_pmids)
-print str(len(set(mesh)))+' different mesh headings'
-
-i=0
-print 'Top 5'
-
-with open('../data/mesh.csv', 'wb') as csvfile:
-    mesh_file = csv.writer(csvfile)
-    for w in sorted(freq, key=freq.get, reverse=True):
-        if i<5:
-            print w, freq[w]
-            i=i+1
-        #Need to utf-8 encode
-        mesh_file.writerow([w.encode('utf-8'), freq[w]])
+#Do some actual analysis on the data
+#analysis.journals(pmids, papers)
+#analysis.abstracts(pmids, papers)
+#analysis.authors(pmids, papers)
+#analysis.first_authors(pmids, papers)
+analysis.inst(pmids, papers)
+#analysis.mesh(pmids, papers)
--- source/tools.py
+++ source/tools.py
@@ -1,6 +1,130 @@
 #! /usr/bin/env python
 
-def clean_institution(inst):
+#Have a go at tidying up the mess that is first author institution.
+
+def clean_institution_course(inst):
+    import re
+
+    for x in range(0, len(inst)):
+        #print inst[x]
+        
+        #Do email addresses first - my guess is that people are more likely to 
+        #have only one email address than multiple institution addresses...
+        inst[x] = re.sub(r'bbk.ac.uk', r'Birbeck', inst[x])
+        inst[x] = re.sub(r'(?i)bristol.ac.uk|bris.ac.uk', r'University of Bristol', inst[x])
+        inst[x] = re.sub(r'bpmf.ac.uk', r'bpmf.ac.uk\?', inst[x])
+        inst[x] = re.sub(r'cam.ac.uk', r'University of Cambridge', inst[x])
+        inst[x] = re.sub(r'city.ac.uk', r'City University', inst[x])
+        inst[x] = re.sub(r'ex.ac.uk', r'University of Essex', inst[x])
+        inst[x] = re.sub(r'einstein.yu.edu', r'Albert Einstein College of Medicine', inst[x])
+        inst[x] = re.sub(r'gla.ac.uk', r'University of Glasgow', inst[x])
+        inst[x] = re.sub(r'harvard.edu', r'Harvard University', inst[x])
+        inst[x] = re.sub(r'jhsph.edu|jhmi.edu', r'Johns Hopkins University', inst[x])
+        inst[x] = re.sub(r'kcl.ac.uk', r'KCL', inst[x])
+        inst[x] = re.sub(r'liverpool.ac.uk', r'University of Liverpool', inst[x])
+        inst[x] = re.sub(r'(?i)lshtm.ac.uk', r'LSHTM', inst[x])
+        inst[x] = re.sub(r'nottingham.ac.uk', r'University of Nottingham', inst[x])
+        inst[x] = re.sub(r'ox.ac.uk', r'University of Oxford', inst[x])
+        inst[x] = re.sub(r'pds.ac.uk', r'Peninsula College of Medicine & Dentistry', inst[x])
+        inst[x] = re.sub(r'qmw.ac.uk|qmul', r'Queen Mary University London', inst[x])
+        inst[x] = re.sub(r'qub.ac.uk', r'Queens University', inst[x])
+        inst[x] = re.sub(r'sanger.ac.uk', r'Sanger Institute', inst[x])
+        inst[x] = re.sub(r'soton.ac.uk', r'University of Southampton', inst[x])
+        inst[x] = re.sub(r'ucl.ac.uk', r'UCL', inst[x])
+        inst[x] = re.sub(r'uwe.ac.uk', r'UWE', inst[x])
+
+
+        #Do the UK institution addresses.
+        inst[x] = re.sub(r'^.*University of Aberdeen.*$', r'University of Aberdeen', inst[x])
+        inst[x] = re.sub(r'^.*University of Bath.*$', r'University of Bath', inst[x])
+        inst[x] = re.sub(r'^.Birbeckn.*$', r'Birbeck', inst[x])
+        inst[x] = re.sub(r'^.*University of Birmingham.*$|^.*Birmingham University.*$', r'University of Birmingham', inst[x])
+        inst[x] = re.sub(r'^.*University of Brighton.*$', r'University of Brighton', inst[x])
+        inst[x] = re.sub(r'^.*University of Bristol.*$', r'University of Bristol', inst[x])
+        inst[x] = re.sub(r'^.*Brunel University.*$', r'Brunel University', inst[x])
+        inst[x] = re.sub(r'^.*University of Cambridge.*$', r'University of Cambridge', inst[x])
+        inst[x] = re.sub(r'^.*Cardiff University.*$', r'Cardiff University', inst[x])
+        inst[x] = re.sub(r'^.*City University.*$', r'City University', inst[x])
+        inst[x] = re.sub(r'^.*University of Coventry.*$|^.*Coventry University.*$', r'University of Coventry', inst[x])
+        inst[x] = re.sub(r'^.*DeMontfort University.*$', r'DeMontfort University', inst[x])
+        inst[x] = re.sub(r'^.*University of Dundee.*$', r'University of Dundee', inst[x])
+        inst[x] = re.sub(r'^.*University of Durham.*$', r'University of Durham', inst[x])
+        inst[x] = re.sub(r'^.*University of Edinburgh.*$', r'University of Edinburgh', inst[x])
+        inst[x] = re.sub(r'^.*University of Essex.*$', r'University of Essex', inst[x])
+        inst[x] = re.sub(r'^.*University of Exeter.*$', r'University of Exeter', inst[x])
+        inst[x] = re.sub(r'^.*University of Glasgow.*$', r'University of Glasgow', inst[x])
+        inst[x] = re.sub(r'^.*Imperial.*$', r'Imperial', inst[x])
+        inst[x] = re.sub(r'^.*Institute of Education.*$', r'IoE', inst[x])
+        inst[x] = re.sub(r'^.*King\'?s College,? London.*$', r'KCL', inst[x])
+        inst[x] = re.sub(r'^.*Lancaster University.*$', r'Lancaster University', inst[x])
+        inst[x] = re.sub(r'^.*University of Leeds.*$', r'University of Leeds', inst[x])
+        inst[x] = re.sub(r'^.*University of Leicester.*$', r'University of Leicester', inst[x])
+        inst[x] = re.sub(r'^.*University of Liverpool.*$', r'University of Liverpool', inst[x])
+        inst[x] = re.sub(r'^.*London Metropolitan University.*$', r'London Metropolitan University', inst[x])
+        inst[x] = re.sub(r'^.*University of Manchester.*$', r'University of Manchester', inst[x])
+        inst[x] = re.sub(r'^.*University of Munich.*$', r'Universtiy of Munich', inst[x])
+        inst[x] = re.sub(r'^.*Newcastle University.*$', r'Newcastle University', inst[x])
+        inst[x] = re.sub(r'^.*University of Newcastle.*$', r'Newcastle University', inst[x])
+        inst[x] = re.sub(r'^.*University of Nottingham.*$', r'University of Nottingham', inst[x])
+        inst[x] = re.sub(r'^.*University of Oxford.*$', r'University of Oxford', inst[x])
+        inst[x] = re.sub(r'^.*Peninsula.*$', r'Peninsula College of Medicine & Dentistry', inst[x])
+        inst[x] = re.sub(r'^.*Plymouth.*$', r'Plymouth University', inst[x])
+        inst[x] = re.sub(r'^.*Queen\'s University.*$', r'Queens University', inst[x])
+        inst[x] = re.sub(r'^.*University of Sheffield.*$', r'University of Sheffield', inst[x])
+        inst[x] = re.sub(r'^.*University of Southampton.*$', r'University of Southampton', inst[x])
+        inst[x] = re.sub(r'^.*University of Strathclyde.*$', r'University of Strathclyde', inst[x])
+        inst[x] = re.sub(r'^.*University of Stirling.*$', r'University of Stirling', inst[x])
+        inst[x] = re.sub(r'^.*University of Surrey.*$', r'University of Surrey', inst[x])
+        inst[x] = re.sub(r'^.*UCL.*$|^.*University College London.*$', r'UCL', inst[x])
+        inst[x] = re.sub(r'^.*University of East Anglia.*$', r'UEA', inst[x])
+        inst[x] = re.sub(r'^.*University of Warwick.*$', r'University of Warwick', inst[x])
+        inst[x] = re.sub(r'^.*University of the West of England.*$|uwe.ac.uk', r'UWE', inst[x])
+        inst[x] = re.sub(r'^.*University of York.*$', r'University of York', inst[x])
+
+        #International
+        inst[x] = re.sub(r'^.*23andMe.*$', r'23andMe', inst[x])
+        inst[x] = re.sub(r'^.*University of Adelaide.*$', r'University of Adelaide', inst[x])
+        inst[x] = re.sub(r'^.*University of Alabama.*$', r'University of Alabama', inst[x])
+        inst[x] = re.sub(r'^.*University of Amsterdam.*$', r'University of Amsterdam', inst[x])
+        inst[x] = re.sub(r'^.*Boston University.*$', r'Boston University', inst[x])
+        inst[x] = re.sub(r'^.*University of California.*$', r'University of California', inst[x])
+        inst[x] = re.sub(r'^.*University of Chicago.*$', r'University of Chicago', inst[x])
+        inst[x] = re.sub(r'^.*Emory University.*$', r'Emory University', inst[x])
+        inst[x] = re.sub(r'^.*University of Groningen.*$', r'University of Gronigen', inst[x])
+        inst[x] = re.sub(r'^.*University of Hong Kong.*$', r'University of Hong Kong', inst[x])
+        inst[x] = re.sub(r'^.*Hong Kong Polytechnic University.*$', r'Hong Kong Polytechnic University', inst[x])
+        inst[x] = re.sub(r'^.*Harokopio University.*$', r'Harokopio University', inst[x])
+        inst[x] = re.sub(r'^.*Harvard University.*$', r'Harvard University', inst[x])
+        inst[x] = re.sub(r'^.*Indiana University.*$', r'Indiana University', inst[x])
+        inst[x] = re.sub(r'^.*University of Iowa.*$', r'University of Iowa', inst[x])
+        inst[x] = re.sub(r'^.*McGill University.*$', r'McGill University', inst[x])
+        inst[x] = re.sub(r'^.*University of Melbourne.*$', r'University of Melbourne', inst[x])
+        inst[x] = re.sub(r'^.*University of North Carolina.*$', r'University of North Carolina', inst[x])
+        inst[x] = re.sub(r'^.*NYU.*$', r'NYU', inst[x])
+        inst[x] = re.sub(r'^.*University of Oregon.*$', r'University of Oregon', inst[x])
+        inst[x] = re.sub(r'^.*University of Queensland.*$', r'University of Queensland', inst[x])
+        inst[x] = re.sub(r'^.*University of Rochester.*$', r'University of Rochester', inst[x])
+        inst[x] = re.sub(r'^.*University of Saskatchewan.*$', r'University of Saskatchewan', inst[x])
+        inst[x] = re.sub(r'^.*University of South Australia.*$', r'University of South Australia', inst[x])
+        inst[x] = re.sub(r'^.*University of South Carolina.*$', r'University of South Carolina', inst[x])
+        inst[x] = re.sub(r'^.*University of Sydney.*$', r'University of Sydney', inst[x])
+        inst[x] = re.sub(r'^.*Syracuse University.*$', r'Syracuse University', inst[x])
+        inst[x] = re.sub(r'^.*University of Toronto.*$', r'University of Toronto', inst[x])
+        inst[x] = re.sub(r'^.*Virginia Commonwealth University.*$', r'Virginia Commonwealth University', inst[x])
+        inst[x] = re.sub(r'^.*Yale University.*$', r'Yale University', inst[x])
+        inst[x] = re.sub(r'^.*VU University.*$', r'VU University', inst[x])
+
+        #Some special ALSPAC munging
+        inst[x] = re.sub(r'^.*Oakfield Grove.*$', r'University of Bristol', inst[x])
+        inst[x] = re.sub(r'^.*Canynge Hall.*$', r'University of Bristol', inst[x])
+
+        #Anything left that has bristol in it
+        #inst[x] = re.sub(r'^.*Bristol.*$', r'Bristol MISC', inst[x])
+
+
+#This approach tries to clean individual institutions. I fear there is
+#too much variation.
+def clean_institution_fine(inst):
     import re
     #print "hello"
     #print inst[0]
