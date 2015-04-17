1ff06cc8ffb638db8178e2b89730cb0819e9d126
OllyButters <olly.butters@bristol.ac.uk>
2015-04-17 15:19:41 +0100

    added time stamp to citation cacher

--- source/add/citations.py
+++ source/add/citations.py
@@ -10,10 +10,11 @@ def citations(pmids, papers):
     
     import urllib2
     import json
+    import datetime
     import time
     import csv
     import logging
-    
+        
     api_key='8024d746590aade6be6856a22a734783'
     field='citedby-count'
     url='http://api.elsevier.com/content/search/scopus'
@@ -24,7 +25,9 @@ def citations(pmids, papers):
         with open('../cache/citations.csv', 'rb') as csvfile:
             f = csv.reader(csvfile)
             for row in f:
-                cached_citations[row[0]]=row[1]
+                cached_citations[row[0]]={}
+                cached_citations[row[0]]['citation_count']=row[1]
+                cached_citations[row[0]]['date_downloaded']=row[2]
         csvfile.close()
         logging.info('Citation cache file read in')
     except:
@@ -36,7 +39,7 @@ def citations(pmids, papers):
 
         #read the cache
         try:
-            papers[this_pmid]['Extras']['Citations'] = cached_citations[this_pmid]
+            papers[this_pmid]['Extras']['Citations'] = cached_citations[this_pmid]['citation_count']
             logging.info(str(this_pmid)+' not in citation cache')
         except:
             #Stick in a small nap so we arent hammering the api too much
@@ -52,7 +55,9 @@ def citations(pmids, papers):
                 citations = t['search-results']['entry'][0]['citedby-count']
                 #print citations
                 papers[this_pmid]['Extras']['Citations']=citations
-                cached_citations[this_pmid]= citations
+                cached_citations[this_pmid] = {}
+                cached_citations[this_pmid]['citation_count'] = citations
+                cached_citations[this_pmid]['date_downloaded'] = datetime.datetime.now() 
             except:
                 #there wasnt a number of citations returned, so see if we can catch this.
                 try:
@@ -67,13 +72,15 @@ def citations(pmids, papers):
                     logging.warn('An unexpected error happened getting the citations!')
                     logging.warn(t)
                     print 'An unexpected error happened getting the citations!'
+                    print request_string
                     print t
 
     csvfile = open('../cache/citations.csv', 'wb')
     citation_file =csv.writer(csvfile)
     for this_citation in cached_citations:
         #print str(cached_citations[this_citation])
-        temp = cached_citations[this_citation]
-        citation_file.writerow([this_citation, str(temp)])
+        temp_citation_count = cached_citations[this_citation]['citation_count']
+        temp_date_downloaded = cached_citations[this_citation]['date_downloaded']
+        citation_file.writerow([this_citation, str(temp_citation_count), temp_date_downloaded])
 
 
