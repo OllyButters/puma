70ed7fbcb7155c45ee0280ea65dab8a8c2669614
Thomas Burton <twyburton@outlook.com>
2016-08-08 10:57:37 +0100

    Fixed geocode indentation

--- source/add/geocode.py
+++ source/add/geocode.py
@@ -109,10 +109,10 @@ def geocode(papers, error_log):
                                 cache_file.close()
                             except:
                                 print 'Unable to get geo-data (Google API Quota Reached) ' + this_paper['Extras']['CleanInstitute'] + " (" + str(number_done) + "/" + str(len(papers)) + ")"
-          			error_log.logError( this_paper['Extras']['CleanInstitute'] + " Google API Quota Reached!")
+                                error_log.logError( this_paper['Extras']['CleanInstitute'] + " Google API Quota Reached!")
                     except:
                         print 'Unable to get geo-data (Probably not on Wikidata) ' + this_paper['Extras']['CleanInstitute'] + " (" + str(number_done) + "/" + str(len(papers)) + ")"
-			error_log.logWarning("Insititue " + this_paper['Extras']['CleanInstitute'] + " not on Wikidata")
+                        error_log.logWarning("Insititue " + this_paper['Extras']['CleanInstitute'] + " not on Wikidata")
                 except:
                     print 'Unable to get geo-data (Wikidata Query Failed) ' + this_paper['Extras']['CleanInstitute'] + " (" + str(number_done) + "/" + str(len(papers)) + ")"
                     error_log.logWarning("Wikidata query failed for " + this_paper['Extras']['CleanInstitute'] )
@@ -121,39 +121,9 @@ def geocode(papers, error_log):
 
         except:
             print 'No Clean Institute for ' + this_paper['IDs']['hash'] + " (" + str(number_done) + "/" + str(len(papers)) + ")"
-	    error_log.logError("Clean Institute Missing for " +  this_paper['IDs']['hash'] )
+            error_log.logError("Clean Institute Missing for " +  this_paper['IDs']['hash'] )
 
         number_done += 1
 
     print "locations found: " + str(locations_found) + "/" + str(number_done)
 
-# Read in lat_long lookup file. Should have an inst name and lat long in each row
-#    geocode = {}
-#    lat_long = []
-#    with open('../config/lat_longs.csv', 'rb') as csvfile:
-#        f = csv.reader(csvfile)
-#        for row in f:
-#            try:
-#                # Check it is not a comment string first.
-#                if(re.match('#', row[0])):
-#                    continue
-#
-#               # If there is a second element in this row then carry on
-#                lat_long = {'lat': row[1], 'long': row[2]}
-#                geocode[row[0]] = lat_long
-#            except:
-#                logging.warn('Something went wrong with looking up the lat/long for ' + row[0])
-#
-#    # Actually do the geocoding
-#    for this_paper in papers:
-#
-#        try:
-#            this_paper['Extras']['LatLong'] = geocode[this_paper['Extras']['CleanInstitute']]
-
-#        except:
-#            try:
-#                print 'Did not find a lat-long for ' + this_paper['IDs']['hash'] + ' ' + this_paper['Extras']['CleanInstitute']
-#                logging.warn('Did not find a lat-long for ' + this_paper['IDs']['hash'] + ' ' + this_paper['Extras']['CleanInstitute'])
-#            except:
-#                print 'Did not find a lat-long for ' + this_paper['IDs']['hash']
-#                logging.warn('Did not find a lat-long for ' + this_paper['IDs']['hash'])
