e28453711f5ad3d3ea078ae4018c39a1f35f16c2
OllyButters <olly.butters@newcastle.ac.uk>
2021-04-28 13:26:20 +0100

    tidy up

--- source/add_pubmedID_to_zotero.py
+++ source/add_pubmedID_to_zotero.py
@@ -11,12 +11,14 @@ import os
 
 # Update zotero with a PMID where one can be got from PubMed using the PubMed API.
 # This connects using the details in the config.ini file.
+# run like:
+# ./add_pubmedID_to_zotero.py --config config_whatever.ini
 # This requires READ/WRITE access to the zotero library.
 def main(argv):
     # Lets figure out some paths that everything is relative to
     # global root_dir
-    path_to_papers_py = os.path.abspath(sys.argv[0])
-    root_dir = os.path.dirname(os.path.dirname(path_to_papers_py))
+    path_to_this_file = os.path.abspath(sys.argv[0])
+    root_dir = os.path.dirname(os.path.dirname(path_to_this_file))
     print('Root directory = ' + root_dir)
 
     config.build_config_variables(root_dir)
@@ -32,12 +34,15 @@ def main(argv):
     new_keys = []
     new_keys = zot.papers_keys
 
-    # get all new papers. Note this will not write any to disk until it has got all of them.
+    # get all new papers. Note this will not write anything to disk
     zot.getPapersList(key_list=new_keys)
 
+    num_papers = len(zot)
+    print(str(num_papers) + " to process.")
+
     for num, paper in enumerate(zot.papers):
-        print(num)
-        pprint(paper)
+        print("\nWorking on " + str(num) + "/" + str(num_papers))
+        # pprint(paper)
 
         # Don't care about attachment or note items
         if paper['data']['itemType'] in ('attachment', 'note'):
@@ -60,7 +65,7 @@ def main(argv):
 
             if pmid_data.get('Count') == "1":
                 pmid = pmid_data['IdList'][0]
-                print("PMID data: " + pmid)
+                print("PMID from PubMed: " + pmid)
 
         except ValueError as e:
             error = "Pubmed search for DOI: "+paper['data']['DOI']+" error: "+str(e)
@@ -68,9 +73,6 @@ def main(argv):
         except RuntimeError as e:
             error = "Pubmed search for DOI: "+paper['data']['DOI']+" error: "+str(e)
             print(error)
-
-            # Extra
-            # PMID: ####
 
         try:
             pmid
