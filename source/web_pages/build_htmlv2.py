0a33caa389fb090b800a3e64a23bc161ac2836d7
OllyButters <olly.butters@newcastle.ac.uk>
2021-11-22 14:30:53 +0000

    updated about page

--- source/web_pages/build_htmlv2.py
+++ source/web_pages/build_htmlv2.py
@@ -1005,7 +1005,7 @@ def build_institute_map(papers):
 
     temp += '<h1 id="pagetitle">Publications by UK Institute</h1>'
 
-    temp += '<div id="regions_div" style="width: 900px; height: 500px;"><img src="loading.gif" alt="Loading"></div>'
+    temp += '<div id="regions_div" style="width: 100%; min-width:500px; min-height: 500px;"><img src="loading.gif" alt="Loading"></div>'
     temp += "<p>Data from " + intWithCommas(number_of_points) + " publications</p>"
     html_file.write(temp)
 
@@ -1303,7 +1303,7 @@ def build_abstract_word_cloud(data_from_count):
     # Scale the words so the biggest one is equal to biggest_word_size
     abstract_words = {k: round(v * biggest_word_size / max_count) for k, v in abstract_words}
 
-    for this_word in abstract_words:
+    for this_word in sorted(abstract_words):
         if n > 0:
             d3_word_list += ","
 
@@ -1554,24 +1554,37 @@ def build_help():
 
     temp += '<h1 id="pagetitle">Information</h1>'
 
-    temp += '<h2>Where does the data come from?</h2>'
-    temp += '<h3>Publication Data</h3>'
+    temp += '<ul>'
+    temp += '<li><a href="#where_does_the_data_come_from">Where does the data come from?</a></li>'
+    temp += '<li><a href="#what_des_pubmed_doi_and_api_mean">What does PubMed, DOI and API mean?</a></li>'
+    temp += '<li><a href="#where_does_citation_data_come_from">Where does the citation data come from?</a></li>'
+    temp += '<li><a href="#geo_data">Geolocation data</a></li>'
+    temp += '<li><a href="#altmetric_circles">What are the circles with numbers in them?</a></li>'
+    temp += '<li><a href="#missing_data">Why don\'t some statistics use data from all publications?</a></li>'
+    temp += '<li><a href="#all_keywords_vs_mesh">What\'s the difference between "All keywords" and "Major keywords (MeSH)"?</a>>/li>'
+    temp += '<li><a href="#missing_from_keywords">Why doesn\'t my paper show up when I click on a keyword which describes it?</a></li>'
+    temp += '<li><a href="#publication_dates">What are some publications in a different year to what I would expect?</a></li>'
+    temp += '<li><a href="#i_want_to_know_more">I want to know more!</a></li>'
+    temp += '<li><a href="#how_was_this_funded">How was this project funded?</a></li>'
+    temp += '</ul>'
+
+    temp += '<h2 id="where_does_the_data_come_from">Where does the data come from?</h2>'
     temp += '<p>Data and metadata for the publications are downloaded using the PubMed and DOI APIs.</p>'
 
-    temp += '<h3>What does PubMed, DOI and API mean?</h3>'
+    temp += '<h2 id="what_des_pubmed_doi_and_api_mean">What does PubMed, DOI and API mean?</h2>'
     temp += '<p>PubMed is an online search engine which contains journal articles on lifesciences and biomedical topics. DOI is short for Digital Object Identifier and is the de facto way of referring to journal articles, they look like  <a href="https://doi.org/10.12688/f1000research.25484.2">https://doi.org/10.12688/f1000research.25484.2</a> and <a href="http://doi.org">http://doi.org</a> are in charge of them. An API is an Application Programming Interface, which is a fancy way of saying that computer programs can send and receive data to a service. So here we send and receive data to/from PubMed and doi.org.</p>'
 
-    temp += '<h3>Citations Data</h3>'
-    temp += '<p>Citation data is retrieved from the <a href="https://www.elsevier.com/solutions/scopus">Scopus</a> API provided by Elsevier and from the <a href="https://europepmc.org/">Europe PMC</a> API.</p>'
+    temp += '<h2 id="where_does_citation_data_come_from">Where does the citation data come from?</h2>'
+    temp += '<p>Citation data is retrieved from the <a href="https://www.elsevier.com/solutions/scopus">Scopus</a> API provided by Elsevier. This is cached locally and updated regularly. You can see the last time the citation data was updated in the last updated note at the bottom of the page.</p>'
 
-    temp += '<h3>Geodata</h3>'
-    temp += '<p>The coordinate location and town of institutions is retrieved from the free project <a href="https://www.wikidata.org">Wikidata</a>.</p>'
+    temp += '<h2 id="geo_data">Geolocation data</h2>'
+    temp += '<p>We use the first author\'s postal address and email address to assign a paper to a location. Sometimes this may not exist in the metadata, which is why not all papers will be plotted on the maps. Occasionally a paper indicate the authors wish a different author to be considered the lead author, we cannot process this information, so the first author is used.</p>'
+    temp += '<p>The coordinate location of institutions is retrieved from the free open data project <a href="https://www.wikidata.org">Wikidata</a>.</p>'
 
-    temp += '<h2>What are the circles with numbers in them?</h2>'
+    temp += '<h2 id="altmetric_circles">What are the circles with numbers in them?</h2>'
     temp += '<p>Journal article citations are one way to track how an article is being used. Another way is by considering a wider set of metrics, such as if it is mentioned in news articles, or on social media. This is what <a href"https://www.altmetric.com">https://www.altmetric.com</a> does, and by hovering your mouse over one of the circles (or clicking on it) you can get an overview of where this article is being talked about.</p>'
 
-    temp += "<h2>Why don't some statistics use data from all publications?</h2>"
-
+    temp += '<h2 id="missing_data">Why don\'t some statistics use data from all publications?</h2>'
     temp += '<p>Data can be missing because some publications are very old and the data about it just doesn\'t exist, or sometimes a recent paper may not have the data about it available <em>yet</em>. There are many different ways to track publications'
     temp += ' and prior to the introduction of DOIs in 2000 there was no standard method. This means some metadata on old publications could have been lost or not recorded.</p>'
 
@@ -1579,17 +1592,23 @@ def build_help():
     temp += ' This problem is most obvious for citation data from Scopus and Europe PMC. Although they have a particular publication on record,'
     temp += ' there may be citations for this publication from a journal that they do not index and therefore these will not be in the citation count. This is why citation counts from different sources are not always the same.</p>'
 
-    temp += '<h2>What\' the difference between "All keywords" and "Major keywords (MeSH)"?</h2>'
-    temp += '<p>These keywords are assigned to journal articles when they are published. MeSH (Medical Subject Headings) is a commonly used hierarchical controlled vocabulary. This means that there is a list of well defined terms which are allowed to be used (controlled vocabulary), and they are related to one another (hierarchical) e.g. "finger" belongs to "hand". So the Major Keywords are the broader areas, and all keywords will be more fine grained. </p>'
+    temp += '<h2 id="all_keywords_vs_mesh">What\'s the difference between "All keywords" and "Major keywords (MeSH)"?</h2>'
+    temp += '<p>The <em>All keywords</em> page shows author defined keywords and MeSH terms. MeSH (Medical Subject Headings) is a commonly used hierarchical controlled vocabulary. This means that there is a list of well defined terms which are allowed to be used (controlled vocabulary), and they are related to one another (hierarchical) e.g. "finger" belongs to "hand". So the Major Keywords are the broader areas, and all keywords will be more fine grained. MeSH terms are applied retrospectively to a subset of all papers.</p>'
 
-    temp += '<h2>I want to know more!</h2>'
+    temp += '<h2 id="missing_from_keywords">Why doesn\'t my paper show up when I click on a keyword which describes it?</h2>'
+    temp += '<p>Authors can often add whatever they want for their keywords when they submit a paper, this may result in slightly different words for the same thing being used by different papers. Sometimes the author defined keywords are not present in the metadata.</p>'
+
+    temp += '<h2 id="publication_dates">What are some publications in a different year to what I would expect?</h2>'
+    temp += '<p>How do you define when a paper is published? Is it when it is accepted, or when a pre-print is available, or when it is made available on a journal\'s website, or when it appears in a journal\'s printed volume, or something else? Different journals have different preferences for which they prefer. This will likely mean that some papers appear later here than you may expect them to.</p>'
+
+    temp += '<h2 id="i_want_to_know_more">I want to know more!</h2>'
     temp += '<ul>'
     temp += '<li>The source code is at <a href="https://github.com/OllyButters/puma">https://github.com/OllyButters/puma</a></li>'
     temp += '<li>Some documentation is at <a href="https://github.com/OllyButters/puma/wiki">https://github.com/OllyButters/puma/wiki</a></li>'
     temp += '<li>There is even a paper written about it: <a href="https://f1000research.com/articles/9-1095/v2">https://f1000research.com/articles/9-1095/v2</a></li>'
     temp += '<li>You can talk to us at: <a href="https://twitter.com/DrOllyButters">@DrOllyButters</a>, <a href="https://twitter.com/DrBeccaWilson">@DrBeccaWilson</a> and <a href="https://twitter.com/_hugh_garner_">@_hugh_garner_</a></li></ul>'
 
-    temp += '<h2>Funding</h2>'
+    temp += '<h2 id="how_was_this_funded">How was this project funded?</h2>'
     temp += 'This project has been funded by:'
     temp += '<ul>'
     temp += '<li>CLOSER, whose mission is to maximise the use, value and impact of longitudinal studies. CLOSER is funded by the Economic and Social Research Council (ESRC) and Medical Research Council (MRC) (grant reference: ES/K000357/1).</li>'
