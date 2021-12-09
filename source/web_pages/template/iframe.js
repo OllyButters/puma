347b0843f9bf7af6f6f36d33f9dce11e91c7cd18
OllyButters <olly.butters@newcastle.ac.uk>
2021-12-09 20:11:42 +0000

    hack-added multiple parent resizes with time lag.

--- source/web_pages/template/iframe.js
+++ source/web_pages/template/iframe.js
@@ -1,14 +1,14 @@
 if ( window.location !== window.parent.location )
 {
-    
     // The page is in an iFrames
     console.log("The page is in an iFrame");
     
-    // Add a listener to post to parent window the size of the screen. This is used
-    // when displayed in an iframe and needs the dimensions to resize
-    window.addEventListener('load', function() {
-        //let message = { height: document.body.scrollHeight, width: document.body.scrollWidth };	
-    
+    function resize_parent() {
+
+        // hide the header
+        var elem = document.querySelector('#header-container');
+        elem.style.display = 'none';
+
         content_height = document.getElementById("wrapper").scrollHeight;
         footer_height = document.getElementsByClassName("foot")[0].scrollHeight;
 
@@ -16,21 +16,25 @@ if ( window.location !== window.parent.location )
 
         // window.top refers to parent window
         window.top.postMessage(message, "*");
-        console.log(message)
+        console.log(message);
 
-        // hide the header
-        var elem = document.querySelector('#header-container');
-        elem.style.display = 'none';
+    }
 
+    // Add a listener to post to parent window the size of the screen. This is used
+    // when displayed in an iframe and needs the dimensions to resize
+    window.addEventListener('load', function() {
+
+        // Call the resize_parent a few times with a gap between them.
+        // It's an ugly hack as theres loads of async calls to external
+        // services which I don't have call backs for. Sorry.
+        resize_parent();
+        setTimeout( function(){ resize_parent(); }, 1000); 
+        setTimeout( function(){ resize_parent(); }, 5000); 
     });
 
 } 
 else {
-        
     // The page is not in an iFrame
     console.log("The page is not in an iFrame");
 }
-
-
-
 
