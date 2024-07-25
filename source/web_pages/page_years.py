d0391ad41ddfbed0983f3bc27c814040f1402953
OllyButters <oliver.butters@gmail.com>
2024-07-25 20:16:06 +0100

    build same way as tags - includes sorting of result

--- source/web_pages/page_years.py
+++ source/web_pages/page_years.py
@@ -1,8 +1,8 @@
 import os
-import sys
 
 from config import config
 from . import common_html as ch
+from . import utils
 
 SITE_SECOND_TITLE = " Publications"
 
@@ -13,47 +13,27 @@ def build_years(papers):
 
     print("\n###HTML papers list###")
 
-    yearly_papers = {}
-    html_file = open(config.html_dir + '/papers/index.html', 'w', encoding='utf-8')
+    years = {}
 
-    temp = ch.build_common_head("../", "")
-    temp += ch.build_common_body('<p id="breadcrumbs"><a href="../index.html">Home</a> &gt; Papers List</p>', "../")
-
-    temp += '<h1 id="pagetitle">Papers List</h1>'
-    # Altmetric include
-    temp += "<script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script>"
-
-    html_file.write(temp)
-    main = "<p>"
-
-    # Build the text needed for each paper
+    # Build a dict of years with a list of each hash (paper) that has this year.
     for this_paper in papers:
         try:
-            # Call draw paper function
-            html = ch.draw_paper(this_paper, "../../")
+            # If this tag is not already in the dict then add it
+            if this_paper['clean']['clean_date']['year'] not in years:
+                years[this_paper['clean']['clean_date']['year']] = list()
+            years[this_paper['clean']['clean_date']['year']].append(this_paper['IDs']['hash'])
+        except Exception:
+            if 'unknown' not in years:
+                years['unknown'] = list()
+            years['unknown'].append(this_paper['IDs']['hash'])
 
-            # Append this paper to the list indexed by the year
-            this_year = this_paper['clean']['clean_date']['year']
-
-            # Make sure there is a dict item for this year
-            if this_year not in yearly_papers:
-                yearly_papers[this_year] = list()
-
-            temp = yearly_papers[this_year]
-            temp.append({this_paper['IDs']['hash']: html})
-            yearly_papers[this_year] = temp
-        except:
-            print('Failing on ' + this_paper['IDs']['hash'])
-            print(sys.exc_info())
 
     # Output the info into an HTML file
     # For each year dict item
-    for this_year in sorted(yearly_papers, reverse=True):
+    for this_year in years.keys():
         # Check there is some data for this year - not all do
-        if len(yearly_papers[this_year]) == 0:
+        if len(years[this_year]) == 0:
             continue
-
-        main += '<a href="' + this_year + '/index.html">' + this_year + '</a><br/> '
 
         if not os.path.exists(config.html_dir + '/papers/' + this_year):
             os.mkdir(config.html_dir + '/papers/' + this_year)
@@ -65,54 +45,29 @@ def build_years(papers):
         temp += '<h1 id="pagetitle">Papers List - ' + this_year + '</h1>'
         temp += "<script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script>"
 
-        temp += '<h2>' + str(len(yearly_papers[this_year])) + ' Publications From ' + this_year + '</h2>'
+        temp += '<h2>' + str(len(years[this_year])) + ' Publications From ' + this_year + '</h2>'
         year_file.write(temp)
-        # This is a list
-        for this_item in yearly_papers[this_year]:
-            temp = list(this_item.values())
-            year_file.write(temp[0])
+
+        for this_paper in utils.sort_hashes_by(papers, years[this_year], 'year'):
+
+            try:
+                # Get paper object
+                paper_obj = None
+                for p in papers:
+                    if this_paper == p['IDs']['hash']:
+                        paper_obj = p
+                        break
+
+                if paper_obj is not None:
+                    this_paper = paper_obj
+
+                # Call draw paper function
+                html = ch.draw_paper(this_paper, "../../")
+
+                year_file.write(html)
+
+            except Exception:
+                pass
 
         temp = ch.build_common_foot("../../")
         year_file.write(temp)
-
-    main += "</p>" + ch.build_common_foot("../../")
-    html_file.write(main)
-
-    # == Publications from unknown years ==
-    if not os.path.exists(config.html_dir + '/papers/unknown'):
-        os.mkdir(config.html_dir + '/papers/unknown')
-    unknown_file = open(config.html_dir + '/papers/unknown/index.html', 'w', encoding='utf-8')
-
-    # Put html together for this page
-    temp = '<!DOCTYPE html><html lang="en-GB">'
-
-    # html head
-    temp += '<head>'
-    temp += '<title>' + SITE_SECOND_TITLE + '</title>'
-    temp += '<link rel="stylesheet" href="../../css/style_main.css">'
-    temp += '<link rel="stylesheet" href="../../css/colour_scheme.css">'
-    temp += '</head>'
-
-    temp += ch.build_common_body('<p id="breadcrumbs"><a href="../../index.html">Home</a> &gt; <a href="../index.html">Papers List</a> &gt; Unknown Year</p>', "../../")
-
-    temp += '<h1 id="pagetitle">Papers List - Unknown Year</h1>'
-    # Altmetric include
-    temp += "<script type='text/javascript' src='https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js'></script>"
-
-    n = 0
-    html = ""
-    for this_paper in papers:
-        try:
-            this_paper['clean']['clean_date']['year']
-        except:
-            html += ch.draw_paper(this_paper, "../../")
-            n += 1
-
-    temp += '<h2>' + str(n) + ' Publications From Unknown Years</h2>'
-    temp += '<h3>Some data may be missing from these publications.</h3>'
-    temp += html
-
-    temp += ch.build_common_foot("../")
-
-    unknown_file.write(temp)
-
